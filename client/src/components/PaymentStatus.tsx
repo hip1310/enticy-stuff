3ee5e43e2a984cf8102939474e187167ad2e57ba
Harsh Patel <hipclem92@gmail.com>
2023-08-29 10:24:13 +0530

    filter ui complete

--- client/src/components/PaymentStatus.tsx
+++ client/src/components/PaymentStatus.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Elements, useStripe } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { Link } from "react-router-dom";
--- client/src/components/contentfulComponents/ProductComponent/ProductContainer.tsx
+++ client/src/components/contentfulComponents/ProductComponent/ProductContainer.tsx
@@ -7,10 +7,10 @@ const ProductContainer = (element: any) => {
   const { products, setSkip, total } = element;
   return (
     <div className="row producttPageMainContainer">
-      <div className="col-2">
+      <div className="col-5 col-sm-3 col-md-3 col-lg-2">
         <ContentfulMainComponent slug={SLUGS.FILTER} />
       </div>
-      <div className="col-10">
+      <div className="col-7 col-sm-9 col-md-9 col-lg-10">
         <div className="row productContainer">
           {products?.map((element: any, index: number) => {
             return (
